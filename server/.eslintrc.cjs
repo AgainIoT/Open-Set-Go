befcc4d3e2b29e8f35ba45127f25612692e76da1
ymw0407 <yunminwo1211@gmail.com>
2023-08-02 01:51:12 +0900

    Init : eslint & prittier 추가

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,27 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+        es6: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2, { MemberExpression: "off", SwitchCase: 1 }],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "func-style": ["error", "expression", { allowArrowFunctions: true }],
+    "no-unused-vars": ["warn", { args: "none", ignoreRestSiblings: true }],
+    "no-trailing-spaces": ["error"],
+    "arrow-parens": ["error", "always"],
+    "eol-last": ["error", "always"],
+    eqeqeq: ["error", "allow-null"],
+  },
+};
 
-    }
-}
+/*
+this .eslintrc.cjs code referenced by expressjs/express github repository
+https://github.com/expressjs/express/blob/master/.eslintrc.yml
+*/
