fddc13291fec253aeb98b2499518c2cdb7c27a08
ymw0407 <yunminwo1211@gmail.com>
2023-08-04 14:10:01 +0900

    Init : client & server separated from Open-Set-Go repository
    - AgainIoT/Open-Set-Go_client repository added by submodule
    - AgainIoT/Open-Set-Go_server reposiotry added by submodule
    
    Signed-off-by: ymw0407 <yunminwo1211@gmail.com>

--- client/src/pages/HomePage.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { COLOR } from "../styles/color";
-import React from "react";
-
-const HomePage = () => {
-  return <div className="homePage"></div>;
-};
-
-export default HomePage;
--- client/src/styles/color.js
+++
@@ -1,4 +0,0 @@
-export const COLOR = {
-  MAIN_WHITE: "#ffffff",
-  MAIN_BLACK: "#050E04",
-};
